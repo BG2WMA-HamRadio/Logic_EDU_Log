f52315c09f3c1ccd92c11425ffa5868b6a56510d
BG2WMA <63766943+BG2WMA-HamRadio@users.noreply.github.com>
2020-07-16 01:10:30 +0800

    Update day_03_01.Request.quote.py

--- 02_Python爬虫/day_03_01.Request.quote.py
+++ 02_Python爬虫/day_03_01.Request.quote.py
@@ -5,15 +5,18 @@
 @Author     :BG2WMA
 @License    : (C) Copyright 2000-2020, BG2WMA
 @time       :2020-07-14 23:49:36
-@project    :CSDN_Python_Lesson_scrapy
 @file       :day_03_01.Request.quote.py
 @software   :PyCharm
 """
+# 导入urllib.parse模块
 import urllib.parse
+
 
 base_url = 'https://www.baidu.com/s?wd='
 kw = input('清输入：')
+# 使用.quote()方法对字符串编码
 kw = urllib.parse.quote(kw)
+# 拼接url
 url = base_url + kw
 print(url)
 
