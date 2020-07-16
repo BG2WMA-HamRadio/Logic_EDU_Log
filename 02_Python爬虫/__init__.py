105515c8089abb6d4a891ddc0b3e5cec94f763a1
BG2WMA <63766943+BG2WMA-HamRadio@users.noreply.github.com>
2020-07-16 10:41:54 +0800

    Update __init__.py

--- 02_Python爬虫/__init__.py
+++ 02_Python爬虫/__init__.py
@@ -5,7 +5,6 @@
 @Author     :BG2WMA
 @License    : (C) Copyright 2000-2020, BG2WMA
 @time       :2020-07-14 20:01:56
-@project    :CSDN_Python_Lesson_scrapy
 @file       :__init__.py.py
 @software   :PyCharm
 """
